828664ba337dae6eae6ab606ddd61c6b910d5556
Surendra Gahlot <surendragahlot57@gmail.com>
2023-09-09 10:30:42 +0530

    build landing page

--- app/page.js
+++ app/page.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <>
+      <div className="main">
+        <div className="nav">
+        <h2>Logo</h2>
 
-      <div id='parent'>
+          <div className="nav-part2">
+            <h4>Store</h4>
+            <h4>Courses</h4>
+            <h4>Career</h4>
+            <h4>Blog</h4>
+            <h4>Account</h4>
+            <i class="ri-menu-3-line"></i>
+            <button>Sign In</button>
+          </div>
+        </div>
 
-      <div className='child1'></div>
-      <div className='child2'></div>
-      <div className='child3'></div>
+        <div className="content">
+        <div className="left">
+          <h1>Learn From the <span>coolest</span> website of India.</h1>
+          <p> Lorem ipsum dolor sit amett dolorsepidatat non proident, sunt in  non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+          <button>Explore Now !</button>
+        </div>
+        <div className="right">
+          <img src="/coding.avif"  alt="myimage"></img>
+        </div>
+        </div>
       </div>
-
     </>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
